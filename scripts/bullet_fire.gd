e0bb65572e2f2cb1aaafbee5f04b9519927cb2ad
YoanAbdoulESEO <yoan.abdoul@reseau.eseo.fr>
2025-10-27 23:55:41 +0100

    Update bullet_fire.gd

--- scripts/bullet_fire.gd
+++ scripts/bullet_fire.gd
@@ -12,4 +12,5 @@ func _physics_process(delta: float) -> void:
 
 
 func _on_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+	if body.name == "Heart":  
+		body.take_damage(1)
